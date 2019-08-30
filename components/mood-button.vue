40c92e8c4e4515656a64643a796e54bbe8905ef0
Mic Black <mic@micslab.com>
2019-08-31 05:16:37 +1000

    Mock up the look and feel using components were helpful

---
+++ components/mood-button.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="mood">
+		<i v-bind:class="currentIcon"></i>
+	</div>
+</template>
+
+<script>
+module.exports = {
+
+	props: {
+		mood: {
+			default: 'neutral',
+		},
+	},
+
+	data: function () {
+		return {
+			currentIcon: 'fa fa-5x fa-question-circle-o color-info',
+			sadIcon: 'fa fa-5x fa-frown-o color-sad',
+			neutralIcon: 'fa fa-5x fa-meh-o color-neutral',
+			happyIcon: 'fa fa-5x fa-smile-o color-happy',
+			loadingIcon: 'fa fa-5x fa-spinner fa-pulse'
+		}
+	},
+
+	methods: {
+	
+	},
+
+	created () {
+		if (this.mood == 'sad') 	this.currentIcon = this.sadIcon
+		if (this.mood == 'neutral') this.currentIcon = this.neutralIcon
+		if (this.mood == 'happy') 	this.currentIcon = this.happyIcon
+	},
+
+}
+</script>
+
+<style scoped>
+.mood {
+	width: 100px;
+	height: 80px;
+	cursor: pointer;
+	padding: 10px;
+	border: 1px solid silver;
+	border-radius: 5%;
+	text-align:center;
+	display:inline-block;
+	background-color:white;
+}
+.mood:not(:last-child) {
+	margin-right:10px;
+}
+.color-info 	{ color: blue; }
+.color-sad 		{ color: red; }
+.color-neutral 	{ color: grey; }
+.color-happy 	{ color: green; }
+</style>
--- index.js
+++ index.js
@@ -7,20 +7,37 @@ const vm = new window.Vue({
 	el: '#app',
 
 	components: {
-		'my-component': load('my-component'),
+		'mood-button': load('mood-button'),
 	},
 
 	data() {
 		return {
-
+			salutation: 'Hello',
 		}
 	},
 
 	methods: {
 
+		salutationByTimeOfDay() {
+			let hour = new Date().getHours()
+			let response = 'Hello'
+
+			if (hour < 12) {
+			  response = 'Good morning'
+			} else if (hour < 18) {
+			  response = 'Good afternoon'
+			} else {
+			  response = 'Good evening'
+			}
+
+			return response
+		},
+
 	},
 
 	created() {
+
+		this.salutation = this.salutationByTimeOfDay()
 
 	}
 
