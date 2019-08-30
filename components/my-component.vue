c9d852c78427fcf1a6deda8546a5981097732080
Mic Black <mic@micslab.com>
2019-08-31 05:08:57 +1000

    Initial commit with just the basics, no specific use cases. Includes all libraries used

---
+++ components/my-component.vue
@@ -0,0 +1,30 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+module.exports = {
+
+	props: {
+
+	},
+
+	data: function () {
+		return {
+
+		}
+	},
+
+	methods: {
+
+	},
+
+	created () {
+
+	},
+
+}
+</script>
+
+<style scoped>
+</style>
